6e06baaa3dc50ca0a7263e0b8c10e897391a63a8
Maria Belen Becerra <belustianabecerra@gmail.com>
2026-05-05 13:12:34 -0400

    feat(domain): add ManhwaEvent entity

--- lib/domain/entities/manhwa_event.dart
+++ lib/domain/entities/manhwa_event.dart
@@ -3,11 +3,13 @@ class ManhwaEvent {
   final String author;
   final String description;
   final double passPrice;
+  final String imagePath;
 
   ManhwaEvent({
     required this.title,
     required this.author,
     required this.description,
     required this.passPrice,
+    required this.imagePath,
   });
 }
